eeca9b216a6433544e5bc7624106603022552b73
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-04-23 12:19:43 +0700

    Final

--- porto/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import "./styles/globals.css";
-import App from "./App.vue";
-import router from "./router";
-import { createHead } from "@vueuse/head";
-const head = createHead();
-createApp(App).use(head).use(router).mount("#app");
